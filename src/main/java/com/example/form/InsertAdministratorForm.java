6f289e0448f8d86298d12f384e84c33c74896fc4
Mizuho Honda <mizuho.honda@rakus-partners.co.jp>
2025-02-04 16:11:42 +0900

    未完成

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,47 +1,55 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
- * @author igamasayuki
- * 
  */
 public class InsertAdministratorForm {
-	/** 名前 */
-	private String name;
-	/** メールアドレス */
-	private String mailAddress;
-	/** パスワード */
-	private String password;
+    /** 名前 */
+    @NotBlank(message = "名前を入力してください")
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    /** メールアドレス */
+    @NotBlank(message = "メールアドレスを入力してください")
+    @Email(message = "メールアドレスの形式が不正です")
+    private String mailAddress;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /** パスワード */
+    @NotBlank(message = "パスワードを入力してください")
+    @Size(min = 6, message = "パスワードは6文字以上で入力してください")
+    private String password;
 
-	public String getMailAddress() {
-		return mailAddress;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setMailAddress(String mailAddress) {
-		this.mailAddress = mailAddress;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getMailAddress() {
+        return mailAddress;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
 
-	@Override
-	public String toString() {
-		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
-				+ "]";
-	}
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password + "]";
+    }
 }
+
